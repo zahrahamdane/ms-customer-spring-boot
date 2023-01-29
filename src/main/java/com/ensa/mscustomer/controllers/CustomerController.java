617c53e37282529a797c5a87add965fe2ac1e7e6
Zahra <hamdane.zahra@gmail.com>
2023-01-30 00:57:46 +0100

    swagger

--- src/main/java/com/ensa/mscustomer/controllers/CustomerController.java
+++ src/main/java/com/ensa/mscustomer/controllers/CustomerController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
